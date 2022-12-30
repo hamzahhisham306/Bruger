970500a443a2e8f6af2acf35ccb215049a7f66e2
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-12-30 17:40:12 +0000

    Burger

---
+++ Bruger2222.js
@@ -0,0 +1,27 @@
+let menu=document.querySelector('#menu-bar');
+let navbar=document.querySelector('.navbar');
+
+menu.addEventListener('click',function(){
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+});
+window.onscroll=()=>{
+
+
+if(window.scrollY>60){
+  document.querySelector('#scroll-top').classList.add('active');
+}
+else {
+  document.querySelector('#scroll-top').classList.remove('active');
+}
+
+}
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
